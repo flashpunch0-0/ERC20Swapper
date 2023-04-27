2494a76f907c4248aff67856e50ac5cd7f886122
Kratik <kratikbohra5@gmail.com>
2023-04-28 01:06:24 +0530

    3rd commit added baic erc20 contract functions like transferfrom and allowance function with a buy function

--- erc20.sol
+++ erc20.sol
@@ -1,35 +1,41 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.5.0 <0.9.0; 
+pragma solidity >=0.5.0 <0.9.0; // ---------------------------------------------------------------------------- // EIP-20: ERC-20 Token Standard // https://eips.ethereum.org/EIPS/eip-20 // -----------------------------------------
 
-interface ERC20Swapper {
-    function balanceOf(address tokenOwner) external view returns(uint balance);
-    event Transfer(address indexed from, address indexed to, uint tokens);
+interface ERC20Swapper { 
+// function declaring
+function totalSupply() external view returns (uint); function balanceOf(address tokenOwner) external view returns (uint balance); function transfer(address to, uint tokens) external returns (bool success);
+function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+function approve(address spender, uint tokens) external returns (bool success);
+function transferFrom(address from, address to, uint tokens) external returns (bool success);
 
+event Transfer(address indexed from, address indexed to, uint tokens);
+event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
-contract Block is ERC20Swapper{
-    string public name = "Block";
+contract Block is ERC20Swapper { 
+    string public name="Block";
     string public constant symbol = "KVB";
-    uint8 public constant decimals = 18;
-// name of the token string public symbol = "KVB";
+    uint8 public constant decimals = 2;
 
-uint public totalSupply;
+uint public override totalSupply;
 address public contractOwner;
-// made balances private so not easily accessible and only the address's balance can be seen of whom you know the address
-mapping(address=>uint) private balances;
+mapping(address=>uint) public balances;
 mapping(address=>mapping(address=>uint)) allowed;
-    uint256 public unitsOneEthCanBuy  = 10;
+uint256 public unitsOneEthCanBuy  = 10;
+
 
 constructor(){
-    totalSupply = 100000;
-    contractOwner = msg.sender;
+    totalSupply=100000;
+    contractOwner=msg.sender;
     balances[contractOwner]=totalSupply;
 }
+
 function balanceOf(address tokenOwner) public view override returns(uint balance){
-    return balances[tokenOwner];  
+    return balances[tokenOwner];
 }
-function transfer(address to,uint tokens) public payable returns(bool success){
+
+function transfer(address to,uint tokens) public override returns(bool success){
     require(balances[msg.sender]>=tokens);
     balances[to]+=tokens; //balances[to]=balances[to]+tokens;
     balances[msg.sender]-=tokens;
@@ -37,40 +43,31 @@ function transfer(address to,uint tokens) public payable returns(bool success){
     return true;
 }
 
+function approve(address spender,uint tokens) public override returns(bool success){
+    require(balances[msg.sender]>=tokens);
+    require(tokens>0);
+    allowed[msg.sender][spender]=tokens;
+    emit Approval(msg.sender,spender,tokens);
+    return true;
+}
+
+function allowance(address tokenOwner,address spender) public view override returns(uint noOfTokens){
+    return allowed[tokenOwner][spender];
+}
+
+function transferFrom(address from,address to,uint tokens) public override returns(bool success){
+    require(allowed[from][to]>=tokens);
+    require(balances[from]>=tokens);
+    balances[from]-=tokens;
+    balances[to]+=tokens;
+    return true;
+}
+
 function sendmoneytocontract() public payable{
     uint256 amount = msg.value*unitsOneEthCanBuy;
     require(balanceOf(contractOwner)>=amount,"Not enough tokens");
-    transfer(msg.sender,amount);
+    transferFrom(contractOwner,msg.sender,amount);
     emit Transfer(contractOwner,msg.sender,amount);
     payable(contractOwner).transfer(msg.value);
 }
-
 }
-
-// contract DEX {
-
-//     ERC20Swapper public token;
-
-//     event Bought(uint256 amount);
-//     event Sold(uint256 amount);
-
-//     constructor() {
-//         token = new Block();
-//     }
-
-//     function buy() payable public {
-//     uint256 amountTobuy = msg.value;
-//     uint256 dexBalance = token.balanceOf(address(this));
-//     require(amountTobuy > 0, "You need to send some ether");
-//     require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
-//     address.transfer(msg.sender, amountTobuy);
-//     emit Bought(amountTobuy);
-// }
-
-
-//     function sell(uint256 amount) public {
-//         // TODO
-//     }
-
-// }
-
